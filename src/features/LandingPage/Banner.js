de08ac25994bc2e36d46a0600a7c6a5530bc63c3
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-03-30 16:31:02 +0530

    Linting & spacing changes

--- src/features/LandingPage/Banner.js
+++ src/features/LandingPage/Banner.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 const Banner = () => {
-    return <div className="banner">
-    <div className="container">
-        <div className="row">
-            <div className="col-md-4">
-                <div className="banner_item align-items-center" style={{ backgroundImage: "url(images/banner_1.jpg)" }}>
-                    <div className="banner_category">
-                        <a href="/categories">women's</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="banner_item align-items-center" style={{ backgroundImage: "url(images/banner_2.jpg)" }}>
-                    <div className="banner_category">
-                        <a href="/categories">accessories's</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="banner_item align-items-center" style={{ backgroundImage: "url(images/banner_3.jpg)" }}>
-                    <div className="banner_category">
-                        <a href="/categoriesml">men's</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	return (
+		<div className="banner">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						<div
+							className="banner_item align-items-center"
+							style={{ backgroundImage: 'url(images/banner_1.jpg)' }}
+						>
+							<div className="banner_category">
+								<a href="/categories">women's</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div
+							className="banner_item align-items-center"
+							style={{ backgroundImage: 'url(images/banner_2.jpg)' }}
+						>
+							<div className="banner_category">
+								<a href="/categories">accessories's</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div
+							className="banner_item align-items-center"
+							style={{ backgroundImage: 'url(images/banner_3.jpg)' }}
+						>
+							<div className="banner_category">
+								<a href="/categoriesml">men's</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
--- src/features/LandingPage/Slider.js
+++ src/features/LandingPage/Slider.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Slider = () => {
-    return <div className="main_slider" style={{ backgroundImage: "url(images/slider_1.jpg)" }}>
-    <div className="container fill_height">
-        <div className="row align-items-center fill_height">
-            <div className="col">
-                <div className="main_slider_content">
-                    <h6>Summer Collection Sale</h6>
-                    <h1>Get up to 30% Off New Arrivals</h1>
-                    <div className="red_button shop_now_button"><a href="/categories">Explore</a></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	return (
+		<div
+			className="main_slider"
+			style={{ backgroundImage: 'url(images/slider_1.jpg)' }}
+		>
+			<div className="container fill_height">
+				<div className="row align-items-center fill_height">
+					<div className="col">
+						<div className="main_slider_content">
+							<h6>Summer Collection Sale</h6>
+							<h1>Get up to 30% Off New Arrivals</h1>
+							<div className="red_button shop_now_button">
+								<a href="/categories">Explore</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Slider;
